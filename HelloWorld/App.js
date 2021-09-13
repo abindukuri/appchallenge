24517226b2663fb522f67a144a438f394286ff68
harvikK <89046322+harvikK@users.noreply.github.com>
2021-09-13 18:10:02 -0400

    homescreen
    fix button

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry,  TouchableOpacity ,Alert} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text></Text>
       <StatusBar style="auto" />
-      <View style  = {styles.container1}>       <img src={require('./Picutures/logonew.png')} />
+      <View style  = {styles.container1}>         <img src={require('./Picutures/logonew.png')} /> 
+      <View style  = {styles.container2}> <img src={require('./Picutures/leaf.png')} />
+      <View style = {styles.button1}>
+        <button>
+         Log in with Email
+        </button>
+      </View>
+      
 </View>
     </View>
+    </View>
+    
+
   );
 }
 
@@ -20,5 +31,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container1:{  
+    flex:3,
+    top:50,
 
+  },
+  container2:{
+  flex:1,
+  top:125,
+},
+button1:{
+  alignItems: 'center',
+  top:-250,
+
+}
 });
