893a24f79d08086bc8ef4ddc2c580d34a8704e91
harvikK <89046322+harvikK@users.noreply.github.com>
2021-09-26 18:49:59 -0400

    main page button
    formatting mess edup

--- HelloWorld/Menu.js
+++ HelloWorld/Menu.js
@@ -12,13 +12,33 @@ export default function Login(props) {
     <Pressable style={styles.buttonprofile} onPress={() => { props.changeView('Welcome'); } }>
        <View style={styles.profileimg}> <img src={require('./Pictures/Profilepic.jpg')} /></View>
     </Pressable>
-    <View style={styles.Welcomecontainer}><Text style={styles.Welcometext}>Welcome Back!</Text></View>
+    <View style={styles.Welcomecontainer}><Text style={styles.Welcometext}>Welcome Back, John</Text></View> 
+    <View style = {styles.subtitlecontainer}> <Text style = {styles.subtitle}> Speech Practice for Today! </Text>  </View>
+    {/*WORDS*/}
     <View style={styles.Words}>
     <Pressable style={styles.Wordcontainter} onPress={() => { props.changeView('Welcome'); } }>
        <View style={styles.Wordimg}> <img src={require('./Pictures/Wordsicon.png')} /></View>
     </Pressable>
     <Text style={styles.wordtext}>Words</Text>
     </View>
+    {/*PHRASES*/}
+
+    <View style={styles.phrases}>
+    <Pressable style={styles.phrasescontainer} onPress={() => { props.changeView('Welcome'); } }>
+       <View style={styles.phrasesimg}> <img src={require('./Pictures/phrasesicon.png')} /></View>
+    </Pressable>
+    <Text style={styles.phrasestext}>Phrases</Text>
+    </View>
+    {/*Sentences*/}
+    <View style={styles.sentences}>
+    <Pressable style={styles.sentencescontainer} onPress={() => { props.changeView('Welcome'); } }>
+       <View style={styles.sentencesimg}> <img src={require('./Pictures/sentencesicon.png')} /></View>
+    </Pressable>
+    <Text style={styles.sentencestext}>Sentences</Text>
+    </View>
+ 
+
+
     </View>
 
   );
@@ -52,15 +72,26 @@ const styles = StyleSheet.create({
     },
 
     Welcomecontainer: {
-        width:'70%',
+        width:'90%',
         alignItems:'left',
         top:-230,
       },
+
   
     Welcometext: {
         fontSize: 30,
         color:'#fff',
         fontFamily: 'Georgia',
+        left  : 25,
+
+  
+      },
+      phrasestext: {
+        fontSize: 30,
+        color:'#fff',
+        fontFamily: 'Georgia',
+        left  : 25,
+
   
       },
 
@@ -68,10 +99,26 @@ const styles = StyleSheet.create({
           width:62,
           height:84,
           color:'#000',
-          top:-200,
-          left:-110
+          top:-180,
+          left:-130
 
       },
+      phrases: {
+        width:62,
+        height:84,
+        color:'#000',
+        top:-264,
+        left:-40
+
+    },
+    sentences: {
+        width:62,
+        height:84,
+        color:'#000',
+        top:0,
+        left:-40
+
+    },
 
       Wordcontainter: {
           width:62,
@@ -81,6 +128,22 @@ const styles = StyleSheet.create({
           alignItems:'center',
 
       },
+      phrasescontainer: {
+        width:62,
+        height:64,
+        borderRadius:10,
+        backgroundColor:'#fff',
+        alignItems:'center',
+
+    },
+    sentencescontainer: {
+        width:62,
+        height:64,
+        borderRadius:10,
+        backgroundColor:'#fff',
+        alignItems:'center',
+
+    },
 
       Wordimg: {
           width:35,
@@ -88,11 +151,49 @@ const styles = StyleSheet.create({
           top:15,
       },
 
+      phrasesimg: {
+        width:35,
+        height:35,
+        top:15,
+    },
+    sentencesimg: {
+        width:35,
+        height:35,
+        top:15,
+    },
+
       wordtext: {
-        fontSize: 16,
+        fontSize: 12,
         color:'#fff',
         fontFamily: 'Georgia',
         textAlign:'center',
-        paddingTopWidth:5,
+        paddingTopWidth:15,
+    },
+    phrasestext: {
+        fontSize: 12,
+        color:'#fff',
+        fontFamily: 'Georgia',
+        textAlign:'center',
+        paddingTopWidth:15,
+    },
+    sentencestext: {
+        fontSize: 12,
+        color:'#fff',
+        fontFamily: 'Georgia',
+        textAlign:'center',
+        paddingTopWidth:15,
+    },
+    subtitle: {
+        fontSize: 15,
+        top : -300,
+        color:'#fff',
+        fontFamily: 'Georgia',
+        left  : 25,
+
+    },
+    subtitlecontainer: {
+        width:'90%',
+        alignItems:'left',
+        top:-217,
     }
 })
